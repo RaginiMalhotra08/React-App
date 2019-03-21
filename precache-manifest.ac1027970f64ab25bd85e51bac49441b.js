4c48336bc4fa0a5a58cc87bd7dc021d1385c4f62
Verser <verser@outlook.com.au>
2019-03-21 11:20:46 +1100

    Updates

---
+++ precache-manifest.ac1027970f64ab25bd85e51bac49441b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c6e2a14a51ae702d1f05b3fd9623e6d",
+    "url": "/static/media/VerserLogo.5c6e2a14.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c5da13e2076283628aa9",
+    "url": "/static/js/main.5d946cb1.chunk.js"
+  },
+  {
+    "revision": "05647f1a403e6ae4da4f",
+    "url": "/static/js/2.1e6110a6.chunk.js"
+  },
+  {
+    "revision": "c5da13e2076283628aa9",
+    "url": "/static/css/main.7dc12524.chunk.css"
+  },
+  {
+    "revision": "05647f1a403e6ae4da4f",
+    "url": "/static/css/2.3332732c.chunk.css"
+  },
+  {
+    "revision": "7b136a2d33084e27d58e911f0d643173",
+    "url": "/index.html"
+  }
+];
